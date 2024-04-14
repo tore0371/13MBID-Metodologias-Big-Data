c782407e22be0c9172047f26ffc5340b31c6e0be
Salva <86080955+tore0371@users.noreply.github.com>
2024-04-14 10:16:13 +0200

    Fin actividad 1

---
+++ notebooks/modelado/Visualización/app_visualizacion.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+# Se realiza la lectura de los datos
+df = pd.read_csv("/Users/salvarodriguezabad/Desktop/13MBID-Metodologias-Big-Data/data/final/datos_finales.csv", sep=";")
+
+# Título del dashboard
+st.write("# 13MBID - Visualización de datos")
+st.write("## Panel de visualización generado sobre los datos de créditos y tarjetas emitidas a clientes de la entidad")
+st.write("----")
+
+# Gráficos
+st.write("### Caracterización de los créditos otorgados")
+
+# Se tienen que agregar las definiciones de gráficos desde la libreta
+creditos_x_objetivo = px.histogram(df, x='objetivo_credito', 
+                                   title='Conteo de créditos por objetivo')
+creditos_x_objetivo.update_layout(xaxis_title='Objetivo del crédito', yaxis_title='Cantidad')
+
+# Se realiza la "impresión" del gráfico en el dashboard
+st.plotly_chart(creditos_x_objetivo)
